b9a249f91fe6a569679419b07027bf89c4792bed
Mauro Correia <mauroac1@outlook.com>
2022-07-25 19:37:18 -0300

    Added Calendar.

--- app/src/main/java/mauro/correia/minutesaliveapp/MainActivity.kt
+++ app/src/main/java/mauro/correia/minutesaliveapp/MainActivity.kt
@@ -1,11 +1,43 @@
 package mauro.correia.minutesaliveapp
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        selectDateBtn.setOnClickListener {view ->
+            clickDatePicker(view)
+
+        }
+    }
+
+    private fun clickDatePicker(view: View){
+
+        val myCalendar = Calendar.getInstance();
+        val year = myCalendar.get(Calendar.YEAR)
+        val month = myCalendar.get(Calendar.MONTH)
+        val day = myCalendar.get(Calendar.DAY_OF_MONTH)
+
+        DatePickerDialog(this,
+            DatePickerDialog.OnDateSetListener{view, selectedYear, selectedMonth, selectedDayOfMonth ->
+                val selectedDate = "$selectedDayOfMonth/${selectedMonth+1}/$selectedYear"
+                selectedDateView.setText(selectedDate)
+
+                val dateFormater = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+                val today = dateFormater.parse("25/07/2022").time;
+                val birthday = dateFormater.parse(selectedDate).time;
+
+                val ageInMinutes = (today - birthday) / 60000;
+                ageInMinutesView.text = "$ageInMinutes"
+
+            }, year, month, day).show()
     }
 }
